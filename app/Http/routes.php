0a1132dd99ef17f4c667a9bf2a890a08a54edc72
Maxim Oleinik <maxim.oleinik@gmail.com>
2015-10-18 18:16:59 +0300

    Auth: Редирект, если авторизован

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,14 +2,17 @@
 
 use Carbon\Carbon;
 
-// Authentication routes...
-Route::get('auth/login',  ['as' => 'auth.login',  'uses'=>'Auth\AuthController@getLogin']);
-Route::post('auth/login', ['as' => 'auth.login',  'uses'=>'Auth\AuthController@postLogin']);
-Route::get('auth/logout', ['as' => 'auth.logout', 'uses'=>'Auth\AuthController@getLogout']);
 
-// Registration routes...
-Route::get('auth/register', ['as' => 'auth.register', 'uses'=>'Auth\AuthController@getRegister']);
-Route::post('auth/register', ['as' => 'auth.register', 'uses'=>'Auth\AuthController@postRegister']);
+
+Route::get('auth/logout', ['as' => 'auth.logout', 'uses'=>'Auth\AuthController@getLogout']);
+Route::group(['middleware' => 'is_authenticated'], function () {
+    // Authentication routes...
+    Route::get('auth/login',  ['as' => 'auth.login',  'uses'=>'Auth\AuthController@getLogin']);
+    Route::post('auth/login', ['as' => 'auth.login',  'uses'=>'Auth\AuthController@postLogin']);
+    // Registration routes...
+    Route::get('auth/register', ['as' => 'auth.register', 'uses'=>'Auth\AuthController@getRegister']);
+    Route::post('auth/register', ['as' => 'auth.register', 'uses'=>'Auth\AuthController@postRegister']);
+});
 
 
 Route::group(['middleware' => 'auth'], function () {
